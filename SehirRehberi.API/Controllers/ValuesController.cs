12f6a8f6941ad05e6e5c78a0a0c46ae302c3773c
Onur Yurdagelen <yurdagelenonur7@gmail.com>
2022-04-11 00:15:14 +0300

    Add project files.

---
+++ SehirRehberi.API/Controllers/ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SehirRehberi.API.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SehirRehberi.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        //GET api/values
+        [HttpGet]
+        public async Task<ActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+        //GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetValueById(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            if (value !=null)
+            {
+                return Ok(value);
+            }
+            return BadRequest("VALUE NOT FOUND!!!");
+        }
+    }
+}
---
+++ SehirRehberi.API/Controllers/citiesController.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SehirRehberi.API.Data;
+using SehirRehberi.API.Dtos;
+using SehirRehberi.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SehirRehberi.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class citiesController : ControllerBase
+    {
+        IAppRepository _appRepository;
+        IMapper _mapper;
+
+        public citiesController(IMapper mapper,IAppRepository appRepository)
+        {
+            _appRepository = appRepository;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public ActionResult GetCities()
+        {
+            var cities = _appRepository.GetCities();
+            var citiesToReturn = _mapper.Map<List<CityForListDto>>(cities);
+            return Ok(citiesToReturn);
+        }
+        [HttpPost]
+        public ActionResult AddCity([FromBody]City city)
+        {
+            _appRepository.Add(city);
+            _appRepository.SaveAll();
+            return Ok(city);
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult GetCityById(int id)
+        {
+            var city = _appRepository.GetCityById(id);
+            var cityToReturn = _mapper.Map<CityForDetailDto>(city);
+            return Ok(cityToReturn);
+        }
+        [HttpGet]
+        [Route("photos/{cityId}")]
+        public ActionResult GetPhotosByCity(int cityId)
+        {
+            var photos = _appRepository.GetPhotosByCity(cityId);
+            if (photos !=null)
+            {
+            return Ok(photos);
+
+            }
+            return NotFound("PHOTOS NOT FOUND!!!");
+        }
+    }
+}
